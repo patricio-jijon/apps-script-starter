c2cffbfba7682432d6cb6c18f6667a2eb6f47f50
patricio-jijon <patricio@nycfirst.org>
2026-09-17 23:55:45 -0400

    Set D3 field trips web app title

--- src/server/webapp.js
+++ src/server/webapp.js
@@ -1,7 +1,8 @@
 export const doGet = () => {
-  const title = 'Google Apps Script';
+  const title = 'D3 STEM Field Trips';
   const fileName = 'index.html';
   return HtmlService.createHtmlOutputFromFile(fileName)
     .setTitle(title)
+    .addMetaTag('viewport', 'width=device-width, initial-scale=1')
     .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.DEFAULT);
 };
