1f58aa940e7a2950f776ec99760f8129ec334f95
patricio-jijon <patricio@nycfirst.org>
2026-09-17 23:55:41 -0400

    Expose D3 field trip web app functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import { getGmailAliases } from './server/gmail.js';
 import { createQueryString, makeQueryString } from './server/http.js';
 import { doGet } from './server/webapp.js';
+import {
+  getAppData,
+  getAvailability,
+  getDatabaseInfo,
+  submitReservation,
+} from './server/fieldTrips.js';
 
-export { createQueryString, doGet, getGmailAliases, makeQueryString };
+export {
+  createQueryString,
+  doGet,
+  getAppData,
+  getAvailability,
+  getDatabaseInfo,
+  getGmailAliases,
+  makeQueryString,
+  submitReservation,
+};
